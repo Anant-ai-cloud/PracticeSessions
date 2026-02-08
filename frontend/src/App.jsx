91f0b56691ab4fed635c85ae73151470fc816ae6
Anant <anantpathak564@gmail.com>
2026-02-09 00:12:23 +0530

    this is fronend in practice session 3

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+
+  
+  return (
+    <>
+    <div className="flex items-center justify-center w-[1500px] h-[700px] bg-red-400">
+    <div className=" w-40 h-15 bg-blue-500">
+      Hello Welcome to the react app
+    </div>
+    </div>
+    
+      
+    </>
+  )
+}
+
+export default App
