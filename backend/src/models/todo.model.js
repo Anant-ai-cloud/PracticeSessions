588cd2de50900ffb0b6542f5d09befab60c51fdc
Anant <anantpathak564@gmail.com>
2026-02-09 20:37:16 +0530

    practice session 4, model creation

---
+++ backend/src/models/todo.model.js
@@ -0,0 +1,35 @@
+import mongoose, { Schema } from "mongoose";
+
+const todoSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        maxlength: 100
+    },
+    description: {
+        type: String,
+        maxlength: 500
+    },
+    dueDate: {
+        type: Date,
+    },
+
+    category: {
+        type: String,
+        enum: ["urgent", "non urgent"],
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+}, { timestamps: true })
+
+todoSchema.index({ dueDate: 1 })
+
+const Todo = mongoose.model("Todo", todoSchema)
+export default Todo;
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from "mongoose"
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        default: "user"
+    }
+},{ timestamps: true })
+
+const User = mongoose.model("User", userSchema)
+
+export default User;
