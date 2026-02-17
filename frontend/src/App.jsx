84da491e77c30ea11b6e3f4b89baa3762805a58c
Anant <anantpathak564@gmail.com>
2026-02-17 13:32:14 +0530

    Practice session 12, darkmode lightmode and enhanced the application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
-import react from "react"
+import react, { useEffect, useState } from "react"
 import { Outlet } from "react-router-dom"
 
 
 function App() {
 
+ const [ Dark, setDark ] = useState(window.matchMedia("(prefers-color-scheme: dark)").matches)
+ console.log(Dark)
+
+  // useEffect(()=>{
+
+  // }, [theme])
   
   return (
     <>
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -7,17 +7,18 @@ function Login() {
   return (
     <div className='relative h-screen'>
       <div className='absolute top-[270px] left-[530px]'>
-        <form >
+        <form className='border p-5 dark:border-white border-black' >
           <div className='m-5'>
-            <input type="email" placeholder='Write Your email' className='w-96 h-10 border border-black' />
+            <input type="email" placeholder='write your email' className='w-96 p-3 h-10 border border-black' />
           </div>
           <div className='m-5'>
-            <input type="password" placeholder='write your password' className='w-96 h-10 border border-black' />
+            <input type="password" placeholder='write your password' className='w-96 p-3 h-10 border border-black' />
           </div>
-          <button className="btn bg-white text-black border-[#e5e5e5]">
-            <svg aria-label="Email icon" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="black"><rect width="20" height="16" x="2" y="4" rx="2"></rect><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path></g></svg>
-            Login with Email
+          <button className="btn bg-[#1A77F2] text-white border-[#005fd8] w-[200px] ml-[80px]">
+            
+            Login
           </button>
+
         </form>
 
       </div>
