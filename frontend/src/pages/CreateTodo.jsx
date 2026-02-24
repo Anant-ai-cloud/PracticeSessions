9aaebf6501d80af0f0786008a6f8c2bb08516fa2
Anant <anantpathak564@gmail.com>
2026-02-24 20:41:25 +0530

    Practice Session 19, CreateTodo component creating, date calendar implemented

--- frontend/src/pages/CreateTodo.jsx
+++ frontend/src/pages/CreateTodo.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from "react-redux"
-import { useForm } from "react-hook-form"
-
+import { useForm, Controller } from "react-hook-form"
+import { DayPicker } from "react-day-picker"
 function CreateTodo() {
 
-  const { register, handleSubmit, formState: { errors } } = useForm()
+  const [date, setDate] = useState()
+
+  const { register, handleSubmit, formState: { errors }, control, watch } = useForm()
 
   return (
     <div>
@@ -20,12 +22,39 @@ function CreateTodo() {
             <input id='title' type="text" className='input w-[400px] border dark:border-white focus:outline-none' />
           </div>
           <div className='mt-3'>
-            <fieldset className="fieldset">
-              <legend className="fieldset-legend mb-1">Description</legend>
-              <textarea className="textarea h-24 w-[400px] focus: outline-none border dark:border-white " placeholder="Bio"></textarea>
-              
-            </fieldset>
+
+            <label htmlFor="desc">Description</label>
+            <textarea id='desc' className="textarea h-24 w-[400px] focus: outline-none border dark:border-white " placeholder="Bio"></textarea>
+
           </div>
+
+          <button popoverTarget="rdp-popover" className="input input-border" style={{ anchorName: "--rdp" } }>
+          {watch("date") ? watch("date").toLocaleDateString() : "Pick a date"}
+           
+           </button> 
+          <div popover="auto" id="rdp-popover" className="dropdown h-50px" style={{ positionAnchor: "--rdp" }}>
+
+
+              <Controller
+              name='date'
+              control={control}
+              render={({ field }) => (
+                <>
+              
+                  <DayPicker
+                    className="react-day-picker"
+                    mode="single"
+                    selected={field.value}
+                    onSelect={field.onChange}
+                  />
+                  
+                </>
+              )}
+            />
+          </div>
+          
+
+
           <div className="modal-action">
             <form method="dialog">
               {/* if there is a button in form, it will close the modal */}
